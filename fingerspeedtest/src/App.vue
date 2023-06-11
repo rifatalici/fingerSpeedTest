1b5ddd3799dae46cb7cd532b89ec4822b8be59f2
Rifat (Ufuk) Alıcı <u7uk.alici@gmail.com>
2023-06-11 19:36:07 +0300

    Klavye hızımızı test edebileceğimiz bir uygulama

---
+++ fingerspeedtest/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <FingerSpeed/>
+</template>
+
+<script>
+import FingerSpeed from '@/components/FingerSpeed.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FingerSpeed
+  }
+}
+</script>
